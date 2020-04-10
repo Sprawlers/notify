b63a512a7dbab3e789d0fa7f223d08b0e7c6a15f
GreenColoredLife <greencoloredlife@gmail.com>
2020-04-10 17:57:37 +0700

    .

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const envFound = dotenv.config({ path: '../.env' })
+if (!envFound) throw new Error("⚠️  Couldn't find .env file")
+
+module.exports = {
+  db_host: process.env.DB_HOST,
+  line: {
+    channelAccessToken: process.env.DEV_CHANNEL_TOKEN,
+    channelSecret: process.env.DEV_CHANNEL_SECRET,
+  },
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Working!")
+const line = require('@line/bot-sdk')
+const config = require('./config')
+const mongoose = require('mongoose')
+
+const mongoDB = config.db_host
+const db = mongoose
+    .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(console.log('Connected to database'))
+    .catch((e) => console.error(e))
+
+const lineConfig = config.line
+const client = new line.Client(lineConfig)
+
+const replyMsg = {
+    type: 'text',
+    text: null
+}
+
